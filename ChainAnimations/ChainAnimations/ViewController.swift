a04b8d991b14244183912f29bf43235bfb0298cd
karthikku <karthik.kumar@citrix.com>
2018-09-18 20:24:09 +0530

    Create project

---
+++ ChainAnimations/ChainAnimations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChainAnimations
+//
+//  Created by Karthik on 18/09/18.
+//  Copyright © 2018 Karthik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
